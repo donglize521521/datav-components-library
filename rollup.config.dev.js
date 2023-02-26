56d032142f1c3f4a4a897083e0d72111ee364fd1
星霜下逆天生 <1486252015@qq.com>
2023-02-26 19:18:27 +0800

    feat:1.切换模块化标准 2.多模块构建

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
 const inputPath = path.resolve(__dirname, './src/index.js');
-const outputPath = path.resolve(__dirname, './dist/imooc.datav.js');
+const outputUmdPath = path.resolve(__dirname, './dist/imooc.datav.js');
+const outputEsPath = path.resolve(__dirname, './dist/imooc.datav.es.js');
 console.log(inputPath)
 
 module.exports = {
   input: inputPath,
-  output: {
-    file: outputPath,
+  output: [{
+    file: outputUmdPath,
     format:'umd',
     name:'imoocDatav'
-  }
+  },{
+    file: outputEsPath,
+    format:'es'
+  }]
 }
