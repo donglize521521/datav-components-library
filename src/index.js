74b0247cc22a3a8122e5f37b0f8f8d149fa14560
星霜下逆天生 <1486252015@qq.com>
2023-02-27 22:01:55 +0800

    删除sam-test-data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-// const data = require('sam-test-data');
-// console.log(data.random(), data.a, data.b);
-
-// module.exports = {}
-// import * as data from 'sam-test-data';
-// console.log(data.random(), data.a, data.b);
-
-import { random, a, b }  from 'sam-test-data';
-console.log(random(), a, b);
 
 export default random
